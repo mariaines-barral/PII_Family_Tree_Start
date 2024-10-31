88b0700b351bb3922123b724113806454181a1f9
Ines Barral <barral.mariaines@gmail.com>
2024-10-31 14:29:21 -0300

    pronto para entregar

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,25 +7,24 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            // Node<int> n1 = new Node<int>(1);
-            // Node<int> n2 = new Node<int>(2);
-            // Node<int> n3 = new Node<int>(3);
-            // Node<int> n4 = new Node<int>(4);
-            // Node<int> n5 = new Node<int>(5);
-            // Node<int> n6 = new Node<int>(6);
-            // Node<int> n7 = new Node<int>(7);
-            //
-            // n1.AddChildren(n2);
-            // n1.AddChildren(n3);
-            //
-            // n2.AddChildren(n4);
-            // n2.AddChildren(n5);
-            //
-            // n3.AddChildren(n6);
-            // n3.AddChildren(n7);
-            //
-            // // visitar el árbol aquí
-
+            Node<int> i1 = new Node<int>(1);
+            Node<int> i2 = new Node<int>(2);
+            Node<int> i3 = new Node<int>(3);
+            Node<int> i4 = new Node<int>(4);
+            Node<int> i5 = new Node<int>(5);
+            Node<int> i6 = new Node<int>(6);
+            Node<int> i7 = new Node<int>(7);
+            
+            i1.AddChildren(i2);
+            i1.AddChildren(i3);
+            
+            i2.AddChildren(i4);
+            i2.AddChildren(i5);
+            
+            i3.AddChildren(i6);
+            i3.AddChildren(i7);
+            
+            
             Person p1 = new Person("Pedro", 76);
             Node<Person> n1 = new Node<Person>(p1);
             
